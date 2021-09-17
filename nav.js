f019dc2f68971dfeafda4f80ff842e33fb0fae82
Rahil Salecha <77575080+rrs12@users.noreply.github.com>
2021-09-17 22:15:47 +0530

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,14 @@
+$('#navbut').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('.nav').addClass('affix');
+    } else {
+        $('.nav').removeClass('affix');
+    }
+});
