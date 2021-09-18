881c586b583e87ec2f030c22b09f36ebf8e9895e
rrs12 <rahilsalecha12@gmail.com>
2021-09-18 22:01:16 +0530

    Update

--- nav.js
+++ nav.js
@@ -1,6 +1,5 @@
 $('#navbut').click(function () {
     $(this).toggleClass('active');
-    console.log("Clicked menu");
     $("#mainListDiv").toggleClass("show_list");
     $("#mainListDiv").fadeIn();
 
